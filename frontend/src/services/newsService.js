a6b11e9f07d925c2611c8c4a3a481e5f68a85209
Abhijeet Singh <curiousabhijeet1@gmail.com>
2025-09-16 18:53:04 +0530

    change rturned data format from backend

--- frontend/src/services/newsService.js
+++ frontend/src/services/newsService.js
@@ -4,11 +4,13 @@ const API_URL = import.meta.env.VITE_API_URL; // from .env
 export async function fetchTopHeadlines() {
   const res = await fetch(`${API_URL}/top`);
   if (!res.ok) throw new Error("Failed to fetch top headlines");
-  return res.json();
+  const data = res.json();
+  return data.articles || [];
 }
 
 export async function fetchCategoryNews(category) {
   const res = await fetch(`${API_URL}/category/${category}`);
   if (!res.ok) throw new Error("Failed to fetch category news");
-  return res.json();
+  const data = res.json();
+  return data.articles || [];
 }
